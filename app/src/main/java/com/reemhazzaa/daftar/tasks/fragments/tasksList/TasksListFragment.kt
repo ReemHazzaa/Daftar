ff48b7040e54f059d3d775091ec9ff1221fbdd3c
Reem Hazza3 <reemhazzaa4@gmail.com>
2021-03-20 17:51:33 +0200

    updates

--- app/src/main/java/com/reemhazzaa/daftar/tasks/fragments/tasksList/TasksListFragment.kt
+++ app/src/main/java/com/reemhazzaa/daftar/tasks/fragments/tasksList/TasksListFragment.kt
@@ -3,25 +3,26 @@ package com.reemhazzaa.daftar.tasks.fragments.tasksList
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.GridLayout
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
 import com.google.android.material.snackbar.Snackbar
 import com.reemhazzaa.daftar.MainActivity
 import com.reemhazzaa.daftar.R
-import com.reemhazzaa.daftar.tasks.data.viewModel.SharedViewModel
-import com.reemhazzaa.daftar.tasks.data.viewModel.TaskViewModel
 import com.reemhazzaa.daftar.databinding.FragmentTasksListBinding
 import com.reemhazzaa.daftar.tasks.data.models.Task
-import com.reemhazzaa.daftar.tasks.utils.SwipeToDelete
+import com.reemhazzaa.daftar.tasks.data.viewModel.SharedViewModel
+import com.reemhazzaa.daftar.tasks.data.viewModel.TaskViewModel
 import com.reemhazzaa.daftar.tasks.fragments.tasksList.adapters.TasksAdapter
+import com.reemhazzaa.daftar.tasks.utils.SwipeToDelete
 import com.reemhazzaa.daftar.tasks.utils.hideVirtualKeyboard
+import jp.wasabeef.recyclerview.animators.LandingAnimator
 
-class TasksListFragment : Fragment() {
+class TasksListFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private var _binding: FragmentTasksListBinding? = null
     private val binding get() = _binding!!
@@ -47,8 +48,11 @@ class TasksListFragment : Fragment() {
 
         binding.tasksRV.apply {
             adapter = listAdapter
-            layoutManager = LinearLayoutManager(requireContext())
+            layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
             swipeToDelete(this)
+            itemAnimator = LandingAnimator().apply {
+                addDuration = 300
+            }
         }
 
         tasksViewModel.getAllData.observe(viewLifecycleOwner, Observer { tasksList ->
@@ -80,20 +84,34 @@ class TasksListFragment : Fragment() {
         )
         snackbar.setAction("Undo") {
             tasksViewModel.insertData(deletedItem)
-            listAdapter.notifyItemChanged(position)
         }
         snackbar.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.tasks_list_menu, menu)
+
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.menu_search -> {}
+        when (item.itemId) {
             R.id.menu_delete_all -> confirmDeleteAll()
-            R.id.menu_sortBy -> {}
+            R.id.menu_priority_high -> tasksViewModel.sortByHighPriority.observe(
+                this,
+                { listAdapter.setList(it) })
+            R.id.menu_priority_low -> tasksViewModel.sortByLowPriority.observe(
+                this,
+                { listAdapter.setList(it) })
+            R.id.menu_linear -> binding.tasksRV.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+            R.id.menu_grid -> binding.tasksRV.apply {
+                layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -102,7 +120,7 @@ class TasksListFragment : Fragment() {
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Delete All Tasks")
         builder.setMessage("Are you sure you want to delete all tasks?")
-        builder.setPositiveButton(getString(R.string.yes)) { _,_ ->
+        builder.setPositiveButton(getString(R.string.yes)) { _, _ ->
             tasksViewModel.deleteAll()
             onOperationSuccess("All tasks successfully deleted")
         }
@@ -119,6 +137,31 @@ class TasksListFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query != null) {
+            searchTasksDatabase(query)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(query: String?): Boolean {
+        if (query != null) {
+            searchTasksDatabase(query)
+        }
+        return true
+    }
+
+    private fun searchTasksDatabase(query: String) {
+        var searchQuery = query
+        searchQuery = "%$searchQuery%"
+
+        tasksViewModel.searchDatabase(searchQuery).observe(this, { list ->
+            list.let {
+                listAdapter.setList(it)
+            }
+        })
     }
 
 }
